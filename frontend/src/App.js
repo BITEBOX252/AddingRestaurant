39a4a8db1efb3616f6fe90a72a5bfba9d8f08eef
BITEBOX252 <boxbite252@gmail.com>
2025-04-08 10:02:45 +0500

    Initial Commit

---
+++ frontend/src/App.js
@@ -0,0 +1,28 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import UserLogin from "./pages/auth/UserLogin";
+import UserRegister from "./pages/auth/UserRegister";
+import PrivateRoute from "./components/PrivateRoute";
+import Dashboard from "./pages/Dashboard";
+import Register from "./pages/Restaurant/Register";
+function App() {
+  
+  
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<UserLogin />} />
+          <Route path="/register" element={<UserRegister />} />
+          <Route path="/restaurant-register" element={<Register />} />
+
+          
+          <Route path="/dashboard" element={<PrivateRoute element={<Dashboard />} />} />
+
+          <Route path="*" element={<h1>Error 404 Page not found !!</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
